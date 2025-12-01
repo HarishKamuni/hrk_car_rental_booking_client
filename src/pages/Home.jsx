72ff6c76b6694aec926c7c26e3b744955f931ffd
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-12-01 16:42:28 +0530

    created newsletter,footer and  completed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../components/Hero';
 import FeaturedSection from '../components/FeaturedSection';
 import Banner from '../components/Banner';
 import Testimonial from '../components/Testimonial';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
@@ -10,7 +12,8 @@ const Home = () => {
       <Hero />
       <FeaturedSection />
       <Banner />
-      <Testimonial/>
+      <Testimonial />
+      <Newsletter />
     </>
   );
 };
