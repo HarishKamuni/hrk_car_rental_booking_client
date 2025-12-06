d8641caa050655c91513fd1b3205b965e0a5de7c
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-12-06 15:35:41 +0530

    completed cars page

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -19,7 +19,7 @@ const CarCard = ({ car }) => {
           alt="car image"
           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
         />
-        {car.isAvailable && (
+        {car.isAvaliable && (
           <p className="absolute top-4 left-4 bg-primery/90 text-white text-xs px-2.5 py-1 rounded-full">
             Available Now
           </p>
